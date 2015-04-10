72ea6373d085d48db659356ad45cbf4f73c1d94f
ricodejong02 <ricodejong02@gmail.com>
2015-04-10 21:57:05 +0200

    2.4.6.1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -715,6 +715,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
+            this.MaximizeBox = false;
 
         }
 
